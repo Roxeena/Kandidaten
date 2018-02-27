a75b521ca8439236c7453634d5632a5bc66cb227
Malin <malin.ejdbo@gmail.com>
2018-02-27 11:48:03 +0100

    Movement
    Moving Klubba now possible with mouse

--- 3DAirHockey/Assets/Scripts/SimpleMovement.cs
+++ 3DAirHockey/Assets/Scripts/SimpleMovement.cs
@@ -8,6 +8,8 @@ public class SimpleMovement : MonoBehaviour
     Vector3 playerSize;
     Vector2 p = new Vector2();
     Camera c;
+    public float speed = 1;
+    Touch finger;
 
     Rigidbody rb;
 
@@ -21,79 +23,21 @@ public class SimpleMovement : MonoBehaviour
 
     }
 
-    //private void OnMouseDrag()
-    //{
-    //    rb.MovePosition((Input.mousePosition));
-    //    Debug.Log(c.ScreenToWorldPoint(Input.mousePosition));
-    //    Debug.Log((Input.mousePosition));
-    //}
-
     void OnMouseDrag()
     {
         float distance_to_screen = c.WorldToScreenPoint(gameObject.transform.position).z;
-        //transform.position = c.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
-        Vector3 temp = c.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
-        transform.position = new Vector3(temp.x, 0.0191f, temp.z);
-       // rb.velocity
+        Vector3 mousePos = c.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+        rb.velocity = ((mousePos - rb.position)*speed);
+    }
 
+    void FixedUpdate()
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //    if (Input.GetMouseButton(0))
-        //    {
-
-        //      //  Debug.Log(Event.current);
-        //        //Debug.Log("mousepos:");
-        //        //Debug.Log(Input.mousePosition);
-        //        //Debug.Log("worldpos:");
-        //        //Debug.Log(c);
-
-        //       // p.x = Event.current.mousePosition.x;
-        //        //p.y = Camera.main.pixelHeight - Event.current.mousePosition.y;
-
-        //        Vector3 mousePos = c.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, c.nearClipPlane) );
-
-        //        //Debug.Log("mousepos:");
-        //        //Debug.Log(mousePos);
-
-        //        if (wasJustClicked)
-        //        {
-        //            //Debug.Log("input.mousepos:");
-        //            //Debug.Log(Input.mousePosition);
-        //            //Debug.Log("worldpos:");
-        //            //Debug.Log(c.ScreenToWorldPoint(Input.mousePosition));
-
-
-
-        //            wasJustClicked = false;
-
-        //            if ((mousePos.x >= transform.position.x && mousePos.x < transform.position.x + playerSize.x ||
-        //            mousePos.x <= transform.position.x && mousePos.x > transform.position.x - playerSize.x) &&
-        //            (mousePos.y >= transform.position.z && mousePos.y < transform.position.z + playerSize.z ||
-        //            mousePos.y <= transform.position.z && mousePos.y > transform.position.z - playerSize.z))
-        //            {
-        //                canMove = true;
-        //                Debug.Log(canMove);
-        //            }
-        //            else
-        //            {
-        //                canMove = false;
-        //                Debug.Log(canMove);
-        //            }
-        //        }
-
-        //        if (canMove)
-        //        {
-
-        //            rb.MovePosition(mousePos);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        wasJustClicked = true;
-        //    }
-        //}
+        
     }
 }
