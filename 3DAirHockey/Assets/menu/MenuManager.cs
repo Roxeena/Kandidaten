ed48e26827a527a50baf3530e5068c2d01752520
ElmQ <elias.elmq@gmail.com>
2018-03-13 15:20:23 +0100

    Simple menu
    Simple menu. Start game functionality implemented

---
+++ 3DAirHockey/Assets/menu/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("3DAirHockey");
+    }
+
+    public void SetMultiplayer(bool isOn)
+    {
+        GameValues.IsMultiplayer = isOn;
+    }
+}
