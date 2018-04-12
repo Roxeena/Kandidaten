838a414570716e04fc688088197fbdb9044eb8d5
Malin <malin.ejdbo@gmail.com>
2018-04-12 15:07:03 +0200

    Shield Pick Up + developer canvas
    Lade till pick up object som aktiverar sköldar, dem håller i 10s. Lade
    all debug info i en developer canvas, den är ej inte bindad till en key.
    Lite prefabs som gör det enklare att se strukturen i unity och rensat
    gamla filer.

---
+++ 3DAirHockey/Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.RotateAround(rb.position, Vector3.up, 30 * Time.deltaTime);
+	}
+}
