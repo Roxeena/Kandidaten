1d33ba797e8a90e58e086041f7273f87a4f8d0b2
Malin <malin.ejdbo@gmail.com>
2018-04-13 11:44:04 +0200

    Developer cnavas
    Developer cnavas can now be activated/deactivated with the D tangent at
    the keyboard. Also now you can ohase through your own shield (test) if
    do not like easily removed.

---
+++ 3DAirHockey/Assets/Scripts/Shield.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+
+    public Collider PlayerCol;
+    private Collider ShieldCol;
+
+    // Use this for initialization
+	void Start () {
+        ShieldCol = GetComponent<Collider>();
+        Physics.IgnoreCollision(ShieldCol, PlayerCol);
+    }
+}
