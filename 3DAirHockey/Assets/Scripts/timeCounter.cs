99bf8de9e9d202e767185ae04bdc68280508edea
Malin <malin.ejdbo@gmail.com>
2018-05-24 12:54:21 +0200

    No miliseconds
    Taken away miliseconds from the timer.

--- 3DAirHockey/Assets/Scripts/timeCounter.cs
+++ 3DAirHockey/Assets/Scripts/timeCounter.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-/* Author: 
- * Last change date: 
+/* Author: Pontus Söderqvist && Malin Ejdbo
+ * Last change date: 2018-05-24
  * Checked by: Malin Ejdbo
  * Date of check: 2018-04-13
- * Comment: Documentation.
+ * Comment: More documentation needed.
+ * Date of correction: 
 */
 
 public class timeCounter : MonoBehaviour {
@@ -110,60 +111,42 @@ public class timeCounter : MonoBehaviour {
                 }
 
                 secondsLeft = (secondsLeft - Time.deltaTime) % 60.0f; //the seconds left are calculated
-
-                if (minutesLeft < 1) //change to ss:ms
+                
+                minutes = Mathf.Floor(minutesLeft).ToString();
+                seconds = Mathf.Floor(secondsLeft).ToString();
+                if (minutesLeft < 10) //add "0" beofre minutes
                 {
-                    miliSeconds = (secondsLeft * 60 - Time.deltaTime * 60) % 60; // claculate milisecodns
-                    minutes = Mathf.Floor(secondsLeft).ToString();
-                    seconds = Mathf.Floor(miliSeconds).ToString();
-                    if (secondsLeft < 10) //add "0" before minutes if secondsLeft < 10
-                    {
-                        if (secondsLeft < 5)
-                        {
-                            countDownTxt.SetActive(true);
-
-                            Light1.color = Color.red;
-                            Light2.color = Color.red;
-                            Light3.color = Color.red;
-                            Light4.color = Color.red;
-
-                            Light1.intensity = 0.5f;
-                            Light2.intensity = 0.5f;
-                            Light3.intensity = 0.5f;
-                            Light4.intensity = 0.5f;
-                           
-                            countDownBlue.text = Mathf.Floor(secondsLeft+1).ToString();
-                            countDownBlue.fontSize = 600;
-                            CountDownRed.text = Mathf.Floor(secondsLeft + 1).ToString();
-                            CountDownRed.fontSize = 600;
-
-                            // countDownTxt.SetActive(false);
-                        }
-                        minutes = string.Concat("0", minutes);
-                        
-                    }
-                    if (miliSeconds < 10) //add "0" before seconds if miliseconds  < 10
-                    {
-                        seconds = string.Concat("0", seconds);                       
-                    }           
-                  
-                     timerTxt.text = string.Concat(string.Concat(minutes, ":"), seconds);  //save the output to the text
+                    minutes = string.Concat("0", minutes);
                 }
-                else // mm:ss
+                if (secondsLeft < 10) // add "0" before seconds
                 {
-                    minutes = Mathf.Floor(minutesLeft).ToString();
-                    seconds = Mathf.Floor(secondsLeft).ToString();
-                    if (minutesLeft < 10) //add "0" beofre minutes
-                    {
-                        minutes = string.Concat("0", minutes);
-                    }
-                    if (secondsLeft < 10) // add "0" before seconds
-                    {
-                        seconds = string.Concat("0", seconds);
-                    }    
-                    
-                     timerTxt.text = string.Concat(string.Concat(minutes, ":"), seconds);     //save the output to the text
+                    seconds = string.Concat("0", seconds);
                 }
+
+                //Start the countdown
+                if (secondsLeft < 5)
+                {
+                    countDownTxt.SetActive(true);
+
+                    Light1.color = Color.red;
+                    Light2.color = Color.red;
+                    Light3.color = Color.red;
+                    Light4.color = Color.red;
+
+                    Light1.intensity = 0.5f;
+                    Light2.intensity = 0.5f;
+                    Light3.intensity = 0.5f;
+                    Light4.intensity = 0.5f;
+
+                    countDownBlue.text = Mathf.Floor(secondsLeft + 1).ToString();
+                    countDownBlue.fontSize = 600;
+                    CountDownRed.text = Mathf.Floor(secondsLeft + 1).ToString();
+                    CountDownRed.fontSize = 600;
+
+                    // countDownTxt.SetActive(false);
+                }
+
+                timerTxt.text = string.Concat(string.Concat(minutes, ":"), seconds);     //save the output to the text
             }
         }
         else //count up
